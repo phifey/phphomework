72729cd4ddeec3426c7d9f4a536d5be7ec1f933c
Nathan Phifer <nephifer@dmacc.edu>
2018-09-01 04:14:55 -0500

    testPhp Commit

---
+++ testGit.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Intro PHP</title>
+</head>
+<body>
+    <h1>Version control software</h1>
+    <p> The updating and configuration management of software, which mainly consists of documents, programs, and websites. 
+    Version control software enables us to go back to previous versions of documents and you can keep your files up-to-date
+    no matter what local machine you are working from. Everything is timestamped in VCS making it easy to know what you
+    did.</p>
+    <h1>add</h1>
+    <p>git "add" adds all modified files in the directory and subdirectories to the next git "commit." This will therefore
+    commit all of the files to your git repo.</p>
+    <h1>commit</h1>
+    <p>git "commit" is used to save changes to your localmachine's repo. </p>
+    <h1>push</h1>
+    <p>git "push" is used to upload files and other content from your local repo. You commit, and then transfer by "pushing."</p>
+    <h1>pull</h1>
+    <p>git "pull" is used to download files and other content from your git repo (not local). It will then update your local repo.</p>
+    <h1>clone</h1>
+    <p>git "clone" is used to clone an existing repository. This can be either local or remote.</p>
+
+    <?php 
+        echo "My printed name: Nathan Phifer";
+    ?>
+</body>
+</html>
