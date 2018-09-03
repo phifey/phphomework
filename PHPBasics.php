d51315c5a5e07f4b0f442379d8989a8b17c93cda
Nathan Phifer <nephifer@dmacc.edu>
2018-09-03 01:51:45 -0500

    PHPBasics Assignment

---
+++ PHPBasics.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>PHP Basics</title>
+</head>
+<body>
+    <?php 
+    $yourName = "Nathan";
+    $num1 = 55;
+    $num2 = 90;
+    $total = $num1 + $num2;
+
+    echo "<h1>PHP Basics</h1>";
+    echo "<p>" . "Number 1 is: " . $num1 . "</p>";
+    echo "<p>" . "Number 2 is: " . $num2 . "</p>";
+    echo "<p>" . "The total is: " . $total . "</p>";
+    ?>
+
+    <h2> <?php echo $yourName; ?></h2>
+</body>
+</html>
